614e9988f4666324df0f19aed28ae68c89027698
Gior26 <37639274+Gior26@users.noreply.github.com>
2023-07-31 12:02:10 +0200

    Fixed map client namespace

--- nav2_simple_commander/nav2_simple_commander/robot_navigator.py
+++ nav2_simple_commander/nav2_simple_commander/robot_navigator.py
@@ -83,13 +83,13 @@ class BasicNavigator(Node):
         self.initial_pose_pub = self.create_publisher(PoseWithCovarianceStamped,
                                                       'initialpose',
                                                       10)
-        self.change_maps_srv = self.create_client(LoadMap, '/map_server/load_map')
+        self.change_maps_srv = self.create_client(LoadMap, f'/{namespace}/map_server/load_map')
         self.clear_costmap_global_srv = self.create_client(
-            ClearEntireCostmap, '/'+namespace+'/global_costmap/clear_entirely_global_costmap')
+            ClearEntireCostmap, f'/{namespace}/global_costmap/clear_entirely_global_costmap')
         self.clear_costmap_local_srv = self.create_client(
-            ClearEntireCostmap, '/'+namespace+'/local_costmap/clear_entirely_local_costmap')
-        self.get_costmap_global_srv = self.create_client(GetCostmap, '/'+namespace+'/global_costmap/get_costmap')
-        self.get_costmap_local_srv = self.create_client(GetCostmap, '/'+namespace+'/local_costmap/get_costmap')
+            ClearEntireCostmap, f'/{namespace}/local_costmap/clear_entirely_local_costmap')
+        self.get_costmap_global_srv = self.create_client(GetCostmap, f'/{namespace}/global_costmap/get_costmap')
+        self.get_costmap_local_srv = self.create_client(GetCostmap, f'/{namespace}/local_costmap/get_costmap')
 
     def destroyNode(self):
         self.destroy_node()
